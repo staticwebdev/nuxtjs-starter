6f7173a7802e7c0964aa7f4d8fcca064aa91d71e
Hannah Zhu <t-hannzhu@microsoft.com>
2022-06-21 00:58:45 -0700

    changed test name

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,6 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
+    testDir: "./tests",
     use: {
         baseURL: "http://localhost:3000/"
     },
