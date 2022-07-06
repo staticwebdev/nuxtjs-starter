5079aa80d00e265de542efa5ab8a1af64819479b
HannahZhuSWE <76960450+HannahZhuSWE@users.noreply.github.com>
2022-07-06 15:38:58 -0700

    Create Playwright test harness (#23)
    * added tests
    
    * fixed tests
    
    * creating a github action to run playwright tests
    
    * changed github action
    
    * changed github action for playwright tests
    
    * changed port for tests
    
    * changed name of github action
    
    * changed name of test file
    
    * changed test name
    
    * changed script in readme
    
    * changed the test readme file name
    
    * updated with recommendations from Fran
    
    * updated github action to run on all branches

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+const config: PlaywrightTestConfig = {
+    testDir: "./tests",
+    use: {
+        baseURL: "http://localhost:3000/"
+    },
+    webServer: {
+        command: 'npm run start',
+        port: 3000,
+        timeout: 120 * 1000,
+        reuseExistingServer: !process.env.CI,
+    },
+};
+export default config;
